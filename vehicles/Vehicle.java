97a27de5731796153234b221e164149d566678da
Andrés <andres.gracia@a20pc13.(none)>
2014-12-17 19:16:18 +0100

    Completació dels constructors

--- vehicles/Vehicle.java
+++ vehicles/Vehicle.java
@@ -1,10 +1,26 @@
 public class Vehicle {
-    private int nRodes;
+    public final int nRodes;
     private String matricula;
     private int anyMatriculacio;
+    
     public Vehicle(int nRodes, String matricula, int anyMatriculacio) {
         this.nRodes = nRodes;
         this.matricula = matricula;
         this.anyMatriculacio = anyMatriculacio;
     }
+    
+    public int getRodes(){ return this.nRodes; }
+    
+    public String getMatricula(){ return this.matricula; }
+    
+    // assigna una nova matricula assignant l'any de matriculació actual
+    public void rematricula(String novaMatricula){
+		int anyActual = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+		rematricula(novaMatricula, anyActual);
+	}
+	
+	public void rematricula(String novaMatricula, int anyMatricula){
+		this.matricula = novaMatricula;
+		this.anyMatriculacio = anyMatricula;
+	}
 }
